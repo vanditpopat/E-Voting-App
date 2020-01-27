7d47df7e99f6156b423f3ebb5a67b20808e16c25
aagam1090 <aagamshah1090@gmail.com>
2020-01-27 23:45:18 +0530

    Added The UI Components

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080050;
-        public static final int end = 0x7f08007d;
-        public static final int gone = 0x7f08008d;
-        public static final int invisible = 0x7f080099;
-        public static final int left = 0x7f08009e;
-        public static final int packed = 0x7f0800ce;
-        public static final int parent = 0x7f0800d0;
-        public static final int percent = 0x7f0800d5;
-        public static final int right = 0x7f0800da;
-        public static final int spread = 0x7f080100;
-        public static final int spread_inside = 0x7f080101;
-        public static final int start = 0x7f080106;
-        public static final int top = 0x7f080128;
-        public static final int wrap = 0x7f080139;
+        public static final int bottom = 0x7f080058;
+        public static final int end = 0x7f080085;
+        public static final int gone = 0x7f080095;
+        public static final int invisible = 0x7f0800a1;
+        public static final int left = 0x7f0800a6;
+        public static final int packed = 0x7f0800d6;
+        public static final int parent = 0x7f0800d8;
+        public static final int percent = 0x7f0800dd;
+        public static final int right = 0x7f0800e2;
+        public static final int spread = 0x7f080108;
+        public static final int spread_inside = 0x7f080109;
+        public static final int start = 0x7f08010e;
+        public static final int top = 0x7f080130;
+        public static final int wrap = 0x7f080141;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/android/gms/base/R.java
@@ -56,21 +56,21 @@ public final class R {
         public static final int common_google_signin_btn_text_light_focused = 0x7f07007c;
         public static final int common_google_signin_btn_text_light_normal = 0x7f07007d;
         public static final int common_google_signin_btn_text_light_normal_background = 0x7f07007e;
-        public static final int googleg_disabled_color_18 = 0x7f070085;
-        public static final int googleg_standard_color_18 = 0x7f070086;
+        public static final int googleg_disabled_color_18 = 0x7f070087;
+        public static final int googleg_standard_color_18 = 0x7f070088;
     }
     public static final class id {
         private id() {}
 
-        public static final int adjust_height = 0x7f080046;
-        public static final int adjust_width = 0x7f080047;
-        public static final int auto = 0x7f08004c;
-        public static final int dark = 0x7f08006e;
-        public static final int icon_only = 0x7f080095;
-        public static final int light = 0x7f08009f;
-        public static final int none = 0x7f0800c6;
-        public static final int standard = 0x7f080105;
-        public static final int wide = 0x7f080137;
+        public static final int adjust_height = 0x7f08004e;
+        public static final int adjust_width = 0x7f08004f;
+        public static final int auto = 0x7f080054;
+        public static final int dark = 0x7f080076;
+        public static final int icon_only = 0x7f08009d;
+        public static final int light = 0x7f0800a7;
+        public static final int none = 0x7f0800ce;
+        public static final int standard = 0x7f08010d;
+        public static final int wide = 0x7f08013f;
     }
     public static final class string {
         private string() {}
